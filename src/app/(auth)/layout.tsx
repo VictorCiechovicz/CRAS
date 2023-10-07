ca977a254ac439589cdf6d95dfb93badf95131bd
VictorCiechovicz <victor09ciechovicz@outlook.com>
2023-10-07 12:48:55 -0300

    fix callapi and auth redirect

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,16 +1,13 @@
-'use client'
 import { SideBar } from '@/src/components/common'
-import { redirect } from 'next/navigation'
-import { useSession } from 'next-auth/react'
 
-export default  function AuthLayout({
+export default function AuthLayout({
   children
 }: {
   children: React.ReactNode
-}) {
-  const session =  useSession()
-  if (!session) redirect('/')
+  }) {
+  
   return (
+    
     <div>
       <SideBar />
       <main className="p-6 sm:ml-[19.375rem]">{children}</main>
