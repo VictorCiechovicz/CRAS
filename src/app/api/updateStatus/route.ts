8242408260314ee783ae5f67760b7d022a2d47ee
VictorCiechovicz <victor09ciechovicz@outlook.com>
2023-10-07 23:37:23 -0300

    fix update status

--- src/app/api/updateStatus/route.ts
+++ src/app/api/updateStatus/route.ts
@@ -24,28 +24,30 @@ export const POST = async () => {
     });
 
     const agora = new Date(Date.now() - (new Date().getTimezoneOffset() * 60000));
+    let updated = 0;
+
     for (const familia of familias) {
       const periodoBeneficio = familia.periodBenefit[0];
 
       if (!periodoBeneficio) continue;
 
-
       const startDate = new Date(periodoBeneficio.startDate);
       const endDate = new Date(periodoBeneficio.endDate);
 
       if (agora > endDate && familia.status !== 'INACTIVE') {
+        updated++;
         await prisma.familys.update({
           where: { id: familia.id },
           data: { status: 'INACTIVE' },
         });
-      } else if (agora > startDate && agora < endDate && familia.status !== 'ACTIVE') {
+      } else if (agora >= startDate && agora <= endDate && familia.status !== 'ACTIVE') {
+        updated++;
         await prisma.familys.update({
           where: { id: familia.id },
           data: { status: 'ACTIVE' },
         });
       }
     }
-
     return NextResponse.json({ message: 'Status das famílias atualizado com sucesso.' });
   } catch (error) {
     console.error(error);
